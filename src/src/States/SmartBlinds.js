f2cfab220804c2f817b68d80924a16d465bc8986
GermanBluefox <dogafox@gmail.com>
2018-06-07 01:11:53 +0200

    (bluefox) fix blinds

--- src/src/States/SmartBlinds.js
+++ src/src/States/SmartBlinds.js
@@ -174,7 +174,7 @@ class SmartBlinds extends SmartGeneric {
 
     getIcon() {
         return (
-            <div key={this.id + '.icon'} style={Object.assign({}, Theme.tile.tileIcon, this.state[this.actualId] !== this.min ? {color: Theme.palette.lampOn} : {})} className="tile-icon">
+            <div key={this.id + '.icon'} style={Object.assign({}, Theme.tile.tileIcon, /*this.state[this.actualId] !== this.min ? {color: Theme.palette.lampOn} : */{}, {left: '1em'})} className="tile-icon">
                 <Icon width={'100%'} height={'100%'} style={{zIndex: 1}}/>
                 {this.state.executing ? <CircularProgress style={{zIndex: 3, position: 'absolute', top: 0, left: 0}} size={Theme.tile.tileIcon.width}/> : null}
                 <div style={{
